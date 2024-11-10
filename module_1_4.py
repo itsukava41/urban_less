415669c342cda8bfdb5a45fa86882551e10b4067
Максим <itsuakva41@yandex.ru>
2024-11-10 16:00:13 +1200

    1

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+kurs = "Puthon"
+count_homework = 12
+spend_times = 1.5
+time_for_one_hw =spend_times/count_homework
+print("Курс : ", kurs, "\nВсего задач: ", count_homework, "\nЗатрачено времени:", spend_times, "\nВремя на одну задачу", time_for_one_hw)
